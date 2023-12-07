fde319588f775788fe66eb794bbf05449974a70a
DatNguyen <bacbia3696@gmail.com>
2023-12-07 23:00:01 +0700

    feat: add neorg

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -7,6 +7,14 @@ return {
       load = {
         ["core.defaults"] = {}, -- Loads default behaviour
         ["core.concealer"] = {}, -- Adds pretty icons to your documents
+        ["core.keybinds"] = {}, -- Adds default keybindings
+        ["core.completion"] = {
+          config = {
+            engine = "nvim-cmp",
+          },
+        }, -- Enables support for completion plugins
+        ["core.journal"] = {}, -- Enables support for the journal module
+        ["core.summary"] = {}, -- Enables support for the summary module
         ["core.dirman"] = { -- Manages Neorg workspaces
           config = {
             workspaces = {
