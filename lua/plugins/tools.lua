77f9365dae3080203a9c0ca213611ee66d18080c
Dat Nguyen <dat.nguyen@silentium.io>
2025-04-16 15:26:04 +0700

    feat: update

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -11,18 +11,18 @@ return {
       watermark = "Leetcode",
     },
   },
-  {
-    "HakonHarnes/img-clip.nvim",
-    event = "VeryLazy",
-    opts = {
-      -- add options here
-      -- or leave it empty to use the default settings
-    },
-    keys = {
-      -- suggested keymap
-      { "<leader>P", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
-    },
-  },
+  -- {
+  --   "HakonHarnes/img-clip.nvim",
+  --   event = "VeryLazy",
+  --   opts = {
+  --     -- add options here
+  --     -- or leave it empty to use the default settings
+  --   },
+  --   keys = {
+  --     -- suggested keymap
+  --     { "<leader>P", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+  --   },
+  -- },
   {
     "mikavilpas/yazi.nvim",
     event = "VeryLazy",
